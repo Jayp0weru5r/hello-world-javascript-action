0c04dad822b8408a8ea1735484a3c0e3dbee1057
ahearnjames_86@hotmail.com <ahearnjames_86@hotmail.com>
2021-04-07 23:34:11 -0400

    updates

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
+const archiver = require('archiver');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('whotogreet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+function createZip(patterns, target, cwd) {
+  return new Promise((resolve, reject) => {
+    const output = fs.createWriteStream(target);
+    const archive = archiver('zip', {zlib: {level: 9}});
+
+    archive.on('error', (err) => reject(err));
+    output.on('close', () => resolve());
+    archive.pipe(output);
+
+    for (let pattern of patterns) {
+      archive.glob(pattern, {cwd: cwd});
+    }
+    archive.finalize();
+  });
 }
+
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const patterns = core.getInput('glob').split(' ');
+        const target = core.getInput('target');
+        const cwd = core.getInput('cwd');
+
+        await createZip(patterns, target, cwd);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
